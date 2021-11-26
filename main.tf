509a6f7f4953e94025bd58c8175c93e57a1f6f79
rmhman <95063424+rmhman@users.noreply.github.com>
2021-11-26 14:31:45 -0500

    Update main.tf
    No eip

--- main.tf
+++ main.tf
@@ -46,10 +46,10 @@ resource "aws_instance" "myInstance" {
 }
 
 # Create Elastic IP address
-resource "aws_eip" "myInstance" {
-  vpc      = true
-  instance = aws_instance.myInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
+#resource "aws_eip" "myInstance" {
+#  vpc      = true
+#  instance = aws_instance.myInstance.id
+#tags= {
+#    Name = "my_elastic_ip"
+#  }
+#}
